3ccda8655c0704ba75fdcbe1a2f75216ad459368
Priya.Prabhu <priya.prabhu.osv@fedex.com>
2018-08-03 16:51:26 +0530

    Component created

--- travel/src/app/app.module.ts
+++ travel/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
