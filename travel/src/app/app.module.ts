3388835ea68c17f92fd0ebda135dfde6a0b4ae68
prabh <hantechnologies@github.com>
2018-08-08 12:04:11 +0530

    created test directive

--- travel/src/app/app.module.ts
+++ travel/src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{ RouterModule, Routes} from "@angular/router";
 
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { TestDirective } from './test.directive';
 
+const Routes : Routes = [
+  { path: "", component: HeaderComponent},
+  { path: "Login", component: LoginComponent },
+  { path: "register", component: RegisterComponent }
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    SidemenuComponent,
+    TestDirective
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(Routes)
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ travel/src/app/test.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, TemplateRef, Input, ViewContainerRef, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appTest]'
+})
+export class TestDirective {
+    constructor(private templateRef: TemplateRef<any>,
+                private viewContainer: ViewContainerRef) {
+  }
+  @Input() set appTest(condition: boolean){
+      if(condition){
+        this.viewContainer.createEmbeddedView(this.templateRef);
+      }else{
+        this.viewContainer.clear();
+      }
+  }
+
+}
