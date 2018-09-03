2d9ec774087838795a1b9558cfcd5b6402d178c0
prabh <hantechnologies@github.com>
2018-09-03 15:53:19 +0530

    hello

---
+++ travel/src/app/dataservice.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DataserviceService {
+
+     country = [{id: 'IND', name: 'India'},
+       {id:'USA', name:'United States of America'},
+       {id:'AFR', name:'Africa'},
+       {id:'CAN', name:'Canada'}
+     ];
+
+
+
+}
